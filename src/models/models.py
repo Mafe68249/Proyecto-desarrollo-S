33b53d18fb9771b08b699e9bd4aa10dde407d807
Maria Fernanda Hincapie <mfhincapie68@ucatolica.edu.co>
2026-06-01 15:32:18 -0500

    eliminar correo

--- src/models/models.py
+++ src/models/models.py
@@ -1,19 +1,19 @@
 from sqlmodel import SQLModel, Field
 from src.models.genero import Genero
 from typing import Optional
-from pydantic import EmailStr
+
 
 class UsuarioCreate(SQLModel):
     nombre: str = Field(min_length=2)
     edad: int = Field(gt=0)
-    correo: EmailStr
+
 
 class Usuario(SQLModel, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str = Field(min_length=2)
     edad: int = Field(gt=0)
-    correo: EmailStr
+
 
 
 
