217a722dad69c0b9bb6c9c42ef790b9f3a9808be
Maria Fernanda Hincapie <mfhincapie68@ucatolica.edu.co>
2026-06-01 15:14:37 -0500

    agregar correo

--- src/models/models.py
+++ src/models/models.py
@@ -1,6 +1,7 @@
 from sqlmodel import SQLModel, Field
 from src.models.genero import Genero
 from typing import Optional
+from pydantic import EmailStr
 
 class UsuarioCreate(SQLModel):
     nombre: str = Field(min_length=2)
@@ -11,6 +12,7 @@ class Usuario(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str = Field(min_length=2)
     edad: int = Field(gt=0)
+    correo: EmailStr
 
 
 
